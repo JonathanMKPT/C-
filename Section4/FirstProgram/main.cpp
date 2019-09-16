f46956fc8da1f37956e8c011ceaa6083ad2f2c77
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-16 17:57:40 -0400

    Setting up codelite to work on project files......cool

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int favourite_number;
+    std::cout<<"Enter your favourite Number between 1-100: ";
+    
+    std::cin>>favourite_number;
+    
+    std::cout<<"Wicked cool number fam!"<<std::endl;
+    
+    
+    return 0;
+}
